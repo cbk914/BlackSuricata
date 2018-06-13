60e3ccef2becad9e82a17c8f8dd0b8b755da7d72
cbk914 <cbk914@users.noreply.github.com>
2018-06-13 18:58:58 +0200

    Installer for Suricata stable v4.0.4 on Debian

---
+++ suricata_debian_installer.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Licensed under GPLv2
+# Copyright cbk914@riseup.net 2018
+
+read -p "Network interface? " interface
+cd /tmp/
+echo "[*] Downloading Suricata Stable version..."
+wget https://www.openinfosecfoundation.org/download/suricata-4.0.4.tar.gz
+echo "[*] Uncompressing..."
+tar zvxf suricata-4.0.4.tar.gz
+echo "[*] Installing dependencies..."
+apt update
+apt-get install build-essential pkg-config libpcre++-dev libyaml-dev libpcap0.8-dev zlib1g-dev libpcre3 libpcre3-dbg libpcre3-dev build-essential libpcap-dev   libnet1-dev libyaml-0-2 libyaml-dev pkg-config zlib1g zlib1g-dev libcap-ng-dev libcap-ng0 make libmagic-dev libjansson-dev libnss3-dev libgeoip-dev liblua5.1-dev libhiredis-dev libevent-dev libnetfilter-queue-dev libnetfilter-queue1 libnetfilter-log-dev libnetfilter-log1 libnfnetlink-dev libnfnetlink0
+echo "[*] Installing Suricata..."
+cd suricata-4.0.4
+./configure --enable-geoip
+make
+make install-full
+ldconfig
+echo "[*] Latest adjustments..."
+# Disable LRO/GRO
+ethtool -K $interface gro off lro off
+
+/usr/local/bin/suricata -c /usr/local/etc/suricata//suricata.yaml -i $interface --
