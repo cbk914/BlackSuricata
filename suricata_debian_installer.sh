d8bfe1b7ac65b38d02ea439f3b5f5fc2143d1106
cbk914 <cbk914@riseup.net>
2020-02-29 00:08:14 +0100

    Minor fixes

--- suricata_debian_installer.sh
+++ suricata_debian_installer.sh
@@ -5,14 +5,14 @@
 read -p "Network interface? " interface
 cd /tmp/
 echo "[*] Downloading Suricata Stable version..."
-wget https://www.openinfosecfoundation.org/download/suricata-4.0.4.tar.gz
+wget https://www.openinfosecfoundation.org/download/suricata-current.tar.gz
 echo "[*] Uncompressing..."
-tar zvxf suricata-4.0.4.tar.gz
+tar zvxf suricata-current.tar.gz
 echo "[*] Installing dependencies..."
 apt update
 apt-get install build-essential pkg-config libpcre++-dev libyaml-dev libpcap0.8-dev zlib1g-dev libpcre3 libpcre3-dbg libpcre3-dev build-essential libpcap-dev   libnet1-dev libyaml-0-2 libyaml-dev pkg-config zlib1g zlib1g-dev libcap-ng-dev libcap-ng0 make libmagic-dev libjansson-dev libnss3-dev libgeoip-dev liblua5.1-dev libhiredis-dev libevent-dev libnetfilter-queue-dev libnetfilter-queue1 libnetfilter-log-dev libnetfilter-log1 libnfnetlink-dev libnfnetlink0
 echo "[*] Installing Suricata..."
-cd suricata-4.0.4
+cd suricata-current
 ./configure --enable-geoip
 make
 make install-full
